746ad9b58316eec28481fffd0c85ae209c992519
victor muasya <vicmwe184@gmail.com>
2024-08-18 07:54:56 +0300

    set up the user app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    phone_code = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    national_id = models.CharField(max_length=50, blank=False, null=False)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups'
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions'
+    )
+
+    def __str__(self):
+        return self.username
+
+    def get_full_phone_number(self):
+        """Concatenate phone code and phone number."""
+        if self.phone_code and self.phone_number:
+            return f"{self.phone_code} {self.phone_number}"
+        return self.phone_number or self.phone_code or ""
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import CustomUser
+from .serializers import CustomUserSerializer
 
-# Create your views here.
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+    def get_queryset(self):
+        """Optionally restricts the returned users to a given user."""
+        queryset = CustomUser.objects.all()
+        # Implement filtering or other query modifications here if needed
+        return queryset
