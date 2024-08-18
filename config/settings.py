69e995ab6141fba9059ddc462fbec92169bce7a7
victor muasya <vicmwe184@gmail.com>
2024-08-18 08:56:18 +0300

    add whitenoise for heroku

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+# Enable WhiteNoise for static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'config.urls'
 
