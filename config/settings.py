769defd17f4e38553294e9c3a21d7424992bb620
victor muasya <vicmwe184@gmail.com>
2024-08-19 13:55:53 +0300

    reset static base

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR_COLLECT_STATIC = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -139,7 +140,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR_COLLECT_STATIC, 'staticfiles')
 STATIC_URL = '/static/'
 
 
